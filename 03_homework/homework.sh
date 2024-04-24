6a4bb74e749ce65f537944e46eafcaf617fa477b
JzfL <167728811+JzfL@users.noreply.github.com>
2024-04-23 22:34:52 -0400

    Update homework.sh

--- 03_homework/homework.sh
+++ 03_homework/homework.sh
@@ -13,16 +13,19 @@ mkdir dir1 dir2 dir3 dir4 dir5
 ls
 # 3. In each directory, how would you create 5 .txt files and write "I love data" into each within the directories?
 touch dir1 dir2 dir3 dir4 dir5/I love data.txt 
+or
+echo "I love data" >> *.txt
 # 4. How would you verify the presence of all 5 files?
-ls dir*
+ls *.{txt}
 # 5. How would you append to one of the existing files " and machine learning!"?
-mv 
+echo "and machine learning" >> I love data.txt
 # 6. How would you verify that the text was indeed appended to the existing file?
-
+ls *.{txt}
 # 7. How would you delete all files except for the one with the appended text?
-
+rm I love data.txt
 # 8. How would you navigate back to the parent directory containing all the directories?
-
+cd ..
 # 9. How would you remove each directory along with its contents?
-
+rm -r
 # 10. How would you verify that all directories and files have been deleted?
+ls
